9d26abddb111da40f8cce8d2575b9b55dd76e505
Lavender Starlight <56975081+faithvoid@users.noreply.github.com>
2025-01-19 00:26:07 -0500

    Update web.py

--- web.py
+++ web.py
@@ -17,18 +17,31 @@ def check_authorization(request):
 
 # Actual code starts here
 
-def jailbreak_function():
+def jailbreak_checkra1n():
     try:
         subprocess.call(['sudo', 'checkra1n'])
         return {"message": "Running checkra1n - please be patient!"}
     except Exception as e:
         return {"error": str(e)}
 
-@app.route('/jailbreak', methods=['GET'])
-def jailbreak():
+@app.route('/checkra1n', methods=['GET'])
+def jailbreak1():
     if not check_authorization(request):
         return jsonify({"error": "Unauthorized"}), 401
-    return jsonify(jailbreak_function())
+    return jsonify(jailbreak_checkra1n())
+
+def jailbreak_palera1n():
+    try:
+        subprocess.call(['sudo', 'palera1n'])
+        return {"message": "Running palera1n - please be patient!"}
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.route('/palera1n', methods=['GET'])
+def jailbreak2():
+    if not check_authorization(request):
+        return jsonify({"error": "Unauthorized"}), 401
+    return jsonify(jailbreak_palera1n())
 
 def bypass_function1():
     try:
@@ -36,6 +49,27 @@ def bypass_function1():
         return {"message": "Running bypass - please be patient!"}
     except Exception as e:
         return {"error": str(e)}
+
+def jailbreak_checkm8():
+    try:
+        subprocess.call(['sudo', 'python3', 'checkm8.py'])
+        return {"message": "Running checkm8 - please be patient!"}
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.route('/checkm8', methods=['GET'])
+def jailbreak3():
+    if not check_authorization(request):
+        return jsonify({"error": "Unauthorized"}), 401
+    return jsonify(jailbreak_checkm8())
+
+def bypass_function1():
+    try:
+        subprocess.call(['sudo', './bypass-1.sh'])
+        return {"message": "Running bypass - please be patient!"}
+    except Exception as e:
+        return {"error": str(e)}
+
 
 @app.route('/bypass1', methods=['GET'])
 def bypass1():
@@ -90,11 +124,13 @@ def android():
 def home():
     return """
     sakurara1n:
-    1. /jailbreak - Jailbreak your device.
-    2. /bypass1 - Bypass your un-jailbroken device!
-    3. /bypass2 - Bypass your jailbroken device!
-    4. /android - Run Project Sandcastle (Android)
-    5. /linux - Run Project Sandcastle (Linux)
+    1. /checkra1n - Jailbreak your device (A7-A11).
+    2. /palera1n - Jailbreak your device (A8-A11 - iOS 15.0 / bridgeOS 5.0)
+    3. /checkm8 - Jailbreak your legacy iOS device (pre-A7 devices)
+    4. /bypass1 - Bypass your un-jailbroken device!
+    5. /bypass2 - Bypass your jailbroken device!
+    6. /android - Run Project Sandcastle (Android)
+    7. /linux - Run Project Sandcastle (Linux)
     """
 
 if __name__ == '__main__':
